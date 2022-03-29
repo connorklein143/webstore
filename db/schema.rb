28f8af7f823065ba08173bdf6ce5168d9487b981
shadow <cklein@WK2TK82102DTMID.academy.usna.edu>
2022-03-29 10:57:36 -0400

    29 MAR

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_151428) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_03_29_145418) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -20,8 +19,8 @@ ActiveRecord::Schema.define(version: 2022_02_09_151428) do
     t.integer "quantity"
     t.decimal "rating"
     t.decimal "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
